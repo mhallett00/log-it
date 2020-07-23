6ad094809ac3b1af2f8a9a631541bc86bc31d40b
Michael Hallett <m.hallett@gmail.com>
2020-07-23 19:24:25 -0400

    Consolidated functionality into App component. Implemented useEffect in order to rerun memberstack's script on rerender.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,36 +1,65 @@
-import React, { useState } from 'react';
-import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
-import Home from './Home';
-import Register from './Register';
-import Login from './Login';
-
+import React, { useState, useEffect } from 'react';
+import './App.scss';
+import Button from 'react-bootstrap/Button';
 
 function App() {
-  return (
-    <div>
-      <Router>
-        <div className="App">
-          <Switch>
-            <Route path="/login">
-              <Login />
-            </Route>
-            <Route path="/register">
-              <Register />
-            </Route>
-            <Route path="/">
-              <Home />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
+	// Sets a logged in user based on the local memberstack token.
+	const [user, setUser] = useState(
+		localStorage.getItem('memberstack') &&
+			JSON.parse(localStorage.getItem('memberstack')).email
+			? JSON.parse(localStorage.getItem('memberstack')).email
+			: ''
+	);
 
-    </div>
-  );
+	const logout = () => {
+		localStorage.removeItem('memberstack');
+		localStorage.removeItem('ms_expire');
+		setUser('');
+	};
+
+	// Loads the memberstack script on each rerender. This allows a user to logout and then log back in without needing to refresh the page.
+	useEffect(() => {
+		const script = document.createElement('script');
+
+		script.src = 'https://api.memberstack.io/static/memberstack.js?custom';
+		script.setAttribute(
+			'data-memberstack-id',
+			'7b5c7bb6c59727f611d3950e7bc0266b'
+		);
+		script.async = true;
+
+		document.body.appendChild(script);
+
+		return () => {
+			document.body.removeChild(script);
+		};
+	});
+
+	// conditionally renders content based on user state.
+	return (
+		<div className="user-container">
+			{user ? (
+				<>
+					<div>Logged in as {user}</div>
+					<Button variant="primary" onClick={() => logout()}>
+						Logout
+					</Button>
+				</>
+			) : (
+				<>
+					<Button variant="primary" href="#/ms/login">
+						Login
+					</Button>
+					<Button
+						variant="primary"
+						data-ms-membership="5f19e5184b7231000461a1ab"
+					>
+						Register
+					</Button>
+				</>
+			)}
+		</div>
+	);
 }
 
 export default App;
